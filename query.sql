47aa2f18a32b33ff4ba5b7ca6486e0f98b6fbf10
Benjamin <bmxnguyen@gmail.com>
2022-03-11 12:08:40 -0500

    Adding sql files

---
+++ query.sql
@@ -0,0 +1,15 @@
+select * from customers
+
+select * from products
+
+select * from review_id_table
+
+select * from vine_table
+
+drop table customers
+
+drop table products
+
+drop table review_id_table
+
+drop table vine_table
